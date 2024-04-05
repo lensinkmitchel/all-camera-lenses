b0ef4f245a26ad6484f6a0d949931eae865c2745
Mitchel Lensink <62740968+lensinkmitchel@users.noreply.github.com>
2024-04-06 01:01:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 // JavaScript for dynamically setting equal heights to cards
-        window.onload = function() {
-            setCardHeights();
-        };
+window.onload = function() {
+    setCardDimensions();
+};
 
-        window.onresize = function() {
-            setCardHeights();
-        };
+window.onresize = function() {
+    setCardDimensions();
+};
 
-        function setCardHeights() {
-            var cards = document.querySelectorAll('.card');
-            var maxHeight = 0;
-            
-            cards.forEach(function(card) {
-                card.style.height = 'auto'; // Reset height to auto to recalculate based on content
-                var cardHeight = card.offsetHeight;
-                if (cardHeight > maxHeight) {
-                    maxHeight = cardHeight;
-                }
-            });
+function setCardDimensions() {
+    var cards = document.querySelectorAll('.card');
+    var maxWidth = 0;
+    var maxHeight = 0;
 
-            cards.forEach(function(card) {
-                card.style.height = maxHeight + 'px'; // Set all cards to the same maximum height
-            });
+    cards.forEach(function(card) {
+        card.style.width = 'auto'; // Reset width to auto to recalculate based on content
+        card.style.height = 'auto'; // Reset height to auto to recalculate based on content
+        var cardWidth = card.offsetWidth;
+        var cardHeight = card.offsetHeight;
+        
+        if (cardWidth > maxWidth) {
+            maxWidth = cardWidth;
         }
+        if (cardHeight > maxHeight) {
+            maxHeight = cardHeight;
+        }
+    });
+
+    cards.forEach(function(card) {
+        card.style.width = maxWidth + 'px'; // Set all cards to the same maximum width
+        card.style.height = maxHeight + 'px'; // Set all cards to the same maximum height
+    });
+}
